69f57d03b5f1f9175fe470aac70e34f386b08045
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-02-03 22:56:15 -0300

    ensure RemoteAuthentication throws UnexpectedError on 400

--- test/data/usecases/authentication/remote_authentication_test.dart
+++ test/data/usecases/authentication/remote_authentication_test.dart
@@ -2,7 +2,9 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:fordevs/domain/helpers/helpers.dart';
 import 'package:fordevs/domain/usecases/usecases.dart';
+ 
 import 'package:fordevs/data/http/http.dart';
 import 'package:fordevs/data/usecases/usecases.dart';
 
@@ -19,7 +21,7 @@ void main() {
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
   });
 
-  test('Should call HttpClient with correct valuesgi', () async {
+  test('Should call HttpClient with correct values', () async {
     final params = AuthenticationParams(email: faker.internet.email(), secret: faker.internet.password());
     await sut.auth(params);
 
@@ -28,6 +30,16 @@ void main() {
       method: 'post',
       body: { 'email': params.email, 'password': params.secret }
     ));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', () async {
+    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+      .thenThrow(HttpError.badRequest);
+
+    final params = AuthenticationParams(email: faker.internet.email(), secret: faker.internet.password());
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 
 }
