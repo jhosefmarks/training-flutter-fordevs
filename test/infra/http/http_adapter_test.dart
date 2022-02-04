f2c9ea628489b2a7b246595450c21e48849cb8e9
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-02-04 02:24:19 -0300

    ensure HttpAdapter calls post without Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -21,7 +21,9 @@ class HttpAdapter {
       'accept': 'application/json'
     };
 
-    await client.post(url, headers: headers, body: jsonEncode(body));
+    final jsonBody = body != null ? jsonEncode(body) : null;
+
+    await client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -50,6 +52,15 @@ void main() {
           'accept': 'application/json'
         },
         body: '{"any_key":"any_value"}'
+      ));
+    });
+
+    test('Should calls post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(
+        any,
+        headers: anyNamed('headers')
       ));
     });
   });
