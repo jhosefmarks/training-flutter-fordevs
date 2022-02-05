dc47936344999810f20ecd867d1aff502fb6a691
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-02-04 23:47:42 -0300

    ensure HttpAdapter throws ServerError if posts throws

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -24,8 +24,12 @@ class HttpAdapter implements HttpClient {
 
     final jsonBody = body != null ? jsonEncode(body) : null;
 
-    if (method == 'post') {
-      response = await client.post(url, headers: headers, body: jsonBody);
+    try {
+      if (method == 'post') {
+        response = await client.post(url, headers: headers, body: jsonBody);
+      }
+    } catch(error) {
+      throw HttpError.serverError;
     }
 
     return _handleResponse(response);
