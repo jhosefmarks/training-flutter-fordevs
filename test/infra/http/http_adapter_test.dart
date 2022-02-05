7f6dc1cfd4f6af430b1fd03bef7fed4246f117e2
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-02-04 23:17:26 -0300

    ensure HttpAdapter throws BadRequestError on 400 with no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -83,6 +83,14 @@ void main() {
       expect(future, null);
     });
 
+    test('Should return BadRequestError if post returns 400 with no data', () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
     test('Should return BadRequestError if post returns 400', () async {
       mockResponse(400);
 
@@ -90,5 +98,7 @@ void main() {
 
       expect(future, throwsA(HttpError.badRequest));
     });
+
+    
   });
 }
