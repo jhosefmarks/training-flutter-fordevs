0bc26e20be55719a6a2a68aa23a75f030e0ea547
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-02-06 23:32:22 -0300

    ensure PageLogin presents error if email is invalid

--- test/ui/pages/page_login_test.dart
+++ test/ui/pages/page_login_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,12 +11,22 @@ class LoginPresenterSpy extends Mock implements LoginPresenter {}
 
 void main() {
   LoginPresenterSpy presenter;
+  StreamController<String> emailErrorController;
   
   Future<void> loadPage(WidgetTester tester) async {
     presenter = LoginPresenterSpy();
+    emailErrorController = StreamController<String>();
+
+    when(presenter.emailErrorStream).thenAnswer((_) => emailErrorController.stream);
+
     final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage);
   }
+
+  tearDown(() {
+    emailErrorController.close();
+  });
+
   testWidgets('Should load with correct initial state', (WidgetTester tester) async {
     await loadPage(tester);
 
@@ -48,5 +60,14 @@ void main() {
     await tester.enterText(find.bySemanticsLabel('Senha'), password);
 
     verify(presenter.validatePassword(password));
+  });
+
+  testWidgets('Should present error if email is invalid', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    emailErrorController.add('any error');
+    await tester.pump();
+
+    expect(find.text('any error'), findsOneWidget);
   });
 }
