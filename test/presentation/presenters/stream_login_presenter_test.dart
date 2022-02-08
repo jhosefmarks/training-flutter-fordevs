fd69ada536b3993b02d014d069021262bb1667c8
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-02-07 21:01:46 -0300

    ensure StreamLoginPresenter emits correct events on InvalidCredentialsError

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,5 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:fordevs/domain/entities/account_entity.dart';
+import 'package:fordevs/domain/helpers/domain_error.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
@@ -29,6 +30,10 @@ void main() {
 
   void mockAuthentication() {
     mockAuthenticationCall().thenAnswer((_) async => AccountEntity(faker.guid.guid()));
+  }
+
+  void mockAuthenticationError(DomainError error) {
+    mockAuthenticationCall().thenThrow(error);
   }
 
   setUp(() {
@@ -125,6 +130,18 @@ void main() {
     sut.validatePassword(password);
 
     expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
+    await sut.auth();
+  });
+
+  test('Should emit correct events on InvalidCredentialError', () async {
+    mockAuthenticationError(DomainError.invalidCredentials);
+
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    expectLater(sut.isLoadingStream, emits(false));
+    sut.mainErrorStream.listen(expectAsync1((error) => expect(error, 'Crendenciais inválidas.')));
 
     await sut.auth();
   });
