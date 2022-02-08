1fd888c5b13849e4cc3751445de42ed491c9222e
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-02-08 00:06:47 -0300

    ensure EmailValidation returns null if email is valid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,5 +1,7 @@
-import 'package:fordevs/validation/protocols/protocols.dart';
+import 'package:faker/faker.dart';
 import 'package:test/test.dart';
+
+import 'package:fordevs/validation/protocols/protocols.dart';
 
 class EmailValidation implements FieldValidation {
   final String field;
@@ -24,5 +26,9 @@ void main() {
 
   test('Shoud return null if email is null', () {
     expect(sut.validate(null), null);
+  });
+
+  test('Shoud return null if email is valid', () {
+    expect(sut.validate(faker.internet.email()), null);
   });
 }
