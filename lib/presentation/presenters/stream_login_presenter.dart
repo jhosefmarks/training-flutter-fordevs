8e393397b38317b9bc1707ebba692f4083705034
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-02-07 21:13:34 -0300

    ensure StreamLoginPresenter will not emit events after dispose

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -23,19 +23,19 @@ class StreamLoginPresenter {
   final Validation validation;
   final Authentication authentication;
 
-  final _controller = StreamController<LoginState>.broadcast();
+  var _controller = StreamController<LoginState>.broadcast();
   var _state = LoginState();
 
 
-  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError).distinct();
-  Stream<String> get passwordErrorStream => _controller.stream.map((state) => state.passwordError).distinct();
-  Stream<String> get mainErrorStream => _controller.stream.map((state) => state.mainError).distinct();
-  Stream<bool> get isFormValidStream => _controller.stream.map((state) => state.isFormValid).distinct();
-  Stream<bool> get isLoadingStream => _controller.stream.map((state) => state.isLoading).distinct();
+  Stream<String> get emailErrorStream => _controller?.stream?.map((state) => state.emailError)?.distinct();
+  Stream<String> get passwordErrorStream => _controller?.stream?.map((state) => state.passwordError)?.distinct();
+  Stream<String> get mainErrorStream => _controller?.stream?.map((state) => state.mainError)?.distinct();
+  Stream<bool> get isFormValidStream => _controller?.stream?.map((state) => state.isFormValid)?.distinct();
+  Stream<bool> get isLoadingStream => _controller?.stream?.map((state) => state.isLoading)?.distinct();
 
   StreamLoginPresenter({ @required this.validation, @required this.authentication });
 
-  _update() => _controller.add(_state);
+  _update() => _controller?.add(_state);
 
   void validateEmail(String email) {
     _state.email = email;
@@ -61,5 +61,10 @@ class StreamLoginPresenter {
 
     _state.isLoading = false;
     _update();
+  }
+
+  void dispose() {
+    _controller?.close();
+    _controller = null;
   }
 }
