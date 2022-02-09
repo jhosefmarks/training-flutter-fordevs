7eedb4346e4b2a5ed7a19e7ee7e6588e507f7ef6
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-02-09 17:27:04 -0300

    transform ValidationBuilder constructor to private

--- lib/main/builders/validation_builder.dart
+++ lib/main/builders/validation_builder.dart
@@ -2,14 +2,16 @@ import '../../validation/protocols/protocols.dart';
 import '../../validation/validators/validators.dart';
 
 class ValidationBuilder {
-  static ValidationBuilder _instance;
 
   String fieldName;
-
   List<FieldValidation> validations = [];
 
+  ValidationBuilder._();
+
+  static ValidationBuilder _instance;
+
   static ValidationBuilder field(String fieldName) {
-    _instance = ValidationBuilder();
+    _instance = ValidationBuilder._();
     _instance.fieldName = fieldName;
 
     return _instance;
