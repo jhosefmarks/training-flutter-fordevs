ab2d2b45e272cc00c3a7ada52c4d392103107eba
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-02-09 15:55:41 -0300

    hide keyboard on tap

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -16,7 +16,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  
+
   @override
   void dispose() {
     super.dispose();
@@ -42,40 +42,51 @@ class _LoginPageState extends State<LoginPage> {
               }
             });
 
-            return SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  LoginHeader(),
-                  Headline1(text: 'Login'),
-                  Padding(
-                    padding: EdgeInsets.all(32.0),
-                    child: Provider<LoginPresenter>(
-                      create: (_) => widget.presenter,
-                      child: Form(
-                        child: Column(
-                          children: <Widget>[
-                            EmailInput(),
-                            Padding(
-                              padding: EdgeInsets.only(top: 8.0, bottom: 32.0),
-                              child: PasswordInput(),
-                            ),
-                            LoginButton(),
-                            FlatButton.icon(
-                              onPressed: () {},
-                              icon: Icon(Icons.person),
-                              label: Text('Criar Conta')
-                            )
-                          ],
+            return GestureDetector(
+              onTap: _hideKeyboard,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    LoginHeader(),
+                    Headline1(text: 'Login'),
+                    Padding(
+                      padding: EdgeInsets.all(32.0),
+                      child: Provider<LoginPresenter>(
+                        create: (_) => widget.presenter,
+                        child: Form(
+                          child: Column(
+                            children: <Widget>[
+                              EmailInput(),
+                              Padding(
+                                padding: EdgeInsets.only(top: 8.0, bottom: 32.0),
+                                child: PasswordInput(),
+                              ),
+                              LoginButton(),
+                              FlatButton.icon(
+                                onPressed: () {},
+                                icon: Icon(Icons.person),
+                                label: Text('Criar Conta')
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             );
           }
         ),
     );
+  }
+
+  void _hideKeyboard() {
+    final currentFocus = FocusScope.of(context);
+
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
   }
 }
