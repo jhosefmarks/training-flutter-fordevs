dc0f4efd7b7c0656609127d5b63e96a40e161e98
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-02-21 23:50:20 -0300

    ensure RemoteAddAccount throws UnexpectedError on 400

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -2,6 +2,7 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:fordevs/domain/helpers/helpers.dart';
 import 'package:fordevs/domain/usecases/usecases.dart';
 
 import 'package:fordevs/data/http/http.dart';
@@ -14,6 +15,14 @@ void main() {
   HttpClientSpy httpClient;
   String url;
   AddAccountParams params;
+
+  PostExpectation mockRequest() => when(httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body'),
+      ));
+
+  void mockHttpError(HttpError error) => mockRequest().thenThrow(error);
 
   setUp(() {
     httpClient = HttpClientSpy();
@@ -40,5 +49,13 @@ void main() {
         'passwordConfirmation': params.passwordConfirmation,
       },
     ));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', () async {
+    mockHttpError(HttpError.badRequest);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
