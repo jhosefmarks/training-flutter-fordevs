6439a47c7170b4cd03bfb52affbd50d1812aa1a1
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-02-25 02:14:21 -0300

    move MinLengthValidation to its own file

--- test/validation/validators/min_length_validation_test.dart
+++ test/validation/validators/min_length_validation_test.dart
@@ -1,20 +1,9 @@
 import 'package:faker/faker.dart';
-import 'package:meta/meta.dart';
 import 'package:test/test.dart';
 
 import 'package:fordevs/presentation/protocols/protocols.dart';
-import 'package:fordevs/validation/protocols/protocols.dart';
 
-class MinLengthValidation implements FieldValidation {
-  final String field;
-  final int size;
-
-  MinLengthValidation({@required this.field, @required this.size});
-
-  ValidationError validate(String value) {
-    return value != null && value.length >= size ? null : ValidationError.invalidField;
-  }
-}
+import 'package:fordevs/validation/validators/validators.dart';
 
 void main() {
   MinLengthValidation sut;
