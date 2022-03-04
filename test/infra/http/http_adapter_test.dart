8871d89b23a630e9e9d682da28e2c9172f31108a
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-03-04 01:40:06 -0300

    ensure HttpAdapter returns data on 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -166,8 +166,22 @@ void main() {
 
       verify(client.get(url, headers: {
         'content-type': 'application/json',
-        'accept': 'application/json',
+        'accept': 'application/gjson',
       }));
+    });
+
+    test('Should return data if get returns 200', () async {
+      final response = await sut.request(url: url, method: 'get');
+
+      expect(response, {'any_key': 'any_value'});
+    });
+
+    test('Should return null if get returns 200 with no data', () async {
+      mockResponse(200, body: '');
+
+      final response = await sut.request(url: url, method: 'get');
+
+      expect(response, null);
     });
   });
 }
