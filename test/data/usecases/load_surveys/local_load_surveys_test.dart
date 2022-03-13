cb274fe1475e226b5e95956dd4b209455ee79509
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-03-13 01:33:36 -0300

    ensure LocalLoadSurveys throws UnexpectedError if cache is null

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -16,7 +16,7 @@ class LocalLoadSurveys {
   Future<List<SurveyEntity>> load() async {
     final data = await fetchCacheStorage.fetch('surveys');
 
-    if (data.isEmpty) {
+    if (data?.isEmpty != false) {
       throw DomainError.unexpected;
     }
 
@@ -87,6 +87,14 @@ void main() {
 
   test('Should throw UnexpectedError if cache is empty', () async {
     mockFetch([]);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if cache is null', () async {
+    mockFetch(null);
 
     final future = sut.load();
 
